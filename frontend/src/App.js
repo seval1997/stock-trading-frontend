80cad809d130d8df4b286bc2d011c2fb01e027b7
Seval Patel <seval.patel1997@gmail.com>
2026-07-18 08:46:48 +0530

    divide dashboard page into two divs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import DashboardHeader from "./components/DashboardHeader/DashboardHeader";
 import Watchlist from "./components/WatchListCard/Watchlist";
+import "./App.css"; // add styles here
 
 function Content() {
     return (
-        <div className="d-flex">
-            <Sidebar />
-            <div className="flex-grow-1">
+        <div className="app-container">
+            {/* Sidebar fixed on the left */}
+            <div className="sidebar-container">
+                <Sidebar />
+            </div>
+
+            {/* Dashboard content on the right */}
+            <div className="dashboard-container">
                 <DashboardHeader />
                 <div className="p-4" style={{ marginTop: "60px" }}>
                     <h2 className="mb-3">Stock Watchlist</h2>
